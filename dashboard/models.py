33f01127bde794b93743686e8b6a31fb036635e4
Tomás Barcellos <tomasbarcellos@gmail.com>
2019-10-22 21:11:43 -0300

    inclui modelo tipico da saída de 'calangohc/balanco'

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Registros(models.Model):
+    ESCOLHAS = (
+        ("Paypal", "paypal"),
+        ("Caixa", "caixa"),
+        ("Mercado pago", "mercado pago")
+    )
+    data = models.DateField()
+    quem = models.CharField(max_length=30)
+    origem = models.CharField(max_length=20,choices=ESCOLHAS)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quem} doou {self.valor} via {self.origem}"
+
